6af3fae74246b647f60ec17e339f02e5190f1adc
mo-raies <raiesmohammadhindoli0786@gmail.com>
2025-05-03 11:38:11 +0530

    added gitignore file

--- socket/socket.js
+++ socket/socket.js
@@ -20,6 +20,7 @@ const userSocketMap = {}; // {userId->socketId}
 
 
 io.on('connection', (socket)=>{
+    console.log('user connected:-',socket.id)
     const userId = socket.handshake.query.userId
     if(userId !== undefined){
         userSocketMap[userId] = socket.id;
